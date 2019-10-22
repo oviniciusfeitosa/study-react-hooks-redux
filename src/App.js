81adc949fc77b13dd9c549e30ccd5ad32cad6af7
vinnyfs89 <viniciusfesil@gmail.com>
2019-10-22 01:30:29 -0300

    [UPDATE] Definição de componente CourseList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
-
+import CourseList from './components/CourseList';
 
 function App() {
   return (
     <Provider store={store}>
-    <div className="App">
-    </div>
+    <CourseList />
     </Provider>
   );
 }
